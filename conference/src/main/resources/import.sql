f73fd72d33911ba2398f0d4bc470e13c8d601973
Kim Jong IL <nijin39@gmail.com>
2015-06-24 05:56:21 +0900

    - 2015. 06. 24 	pom jdk modify 	import.sql add

---
+++ conference/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into groups(group_name, thread_pool_size) values ('GROUP1',5);
+insert into groups(group_name, thread_pool_size) values ('GROUP2',5);
+insert into groups(group_name, thread_pool_size) values ('GROUP3',5);
+insert into groups(group_name, thread_pool_size) values ('GROUP4',5);
+insert into groups(group_name, thread_pool_size) values ('GROUP5',5);
