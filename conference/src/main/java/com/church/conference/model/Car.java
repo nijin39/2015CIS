2d091e59b69ee0399900a74e2f3c4fb4b22075ae
KIM JONG IL <nijin39@gmail.com>
2015-06-25 09:00:12 +0900

    warnnig modify

--- conference/src/main/java/com/church/conference/model/Car.java
+++ conference/src/main/java/com/church/conference/model/Car.java
@@ -10,7 +10,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
