70d0d2969f54f1f11bda35826b71760f2b0c86f3
KIM JONG IL <nijin39@gmail.com>
2015-06-26 09:27:58 +0900

    예약 컨트롤러 추가

--- conference/src/main/java/com/church/conference/controller/AssignController.java
+++ conference/src/main/java/com/church/conference/controller/AssignController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.church.conference.model.CarSaint;
-import com.church.conference.model.Saint;
+import com.church.conference.model.RoomSaint;
 import com.church.conference.service.CarSaintRepository;
-import com.church.conference.service.SaintRepository;
+import com.church.conference.service.RoomSaintRepository;
 
 @RestController
 public class AssignController {
@@ -18,13 +18,26 @@ public class AssignController {
 	@Autowired
 	CarSaintRepository carSaintRepository;
 	
+	@Autowired
+	RoomSaintRepository roomSaintRepository;
+	
 	@RequestMapping(value = "/assign/cars")
-	List<CarSaint> findAll(){
+	List<CarSaint> findAssignCarAll(){
 		return carSaintRepository.findAll();
 	}
 	
 	@RequestMapping(value = "/assign/cars/{saintId}")
-	CarSaint findById(@PathVariable long saintId){
+	CarSaint findAssignCarById(@PathVariable long saintId){
 		return carSaintRepository.findById(saintId);
+	}
+	
+	@RequestMapping(value = "/assign/rooms")
+	List<RoomSaint> findAssignRoomAll(){
+		return roomSaintRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/assign/rooms/{roomSaintId}")
+	RoomSaint findAssignRoomById(@PathVariable long roomSaintId){
+		return roomSaintRepository.findById(roomSaintId);
 	}
 }
---
+++ conference/src/main/java/com/church/conference/service/RoomSaintRepository.java
@@ -0,0 +1,12 @@
+package com.church.conference.service;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.church.conference.model.RoomSaint;
+
+public interface RoomSaintRepository extends CrudRepository<RoomSaint, Long> {
+	RoomSaint findById(long roomSaintId);
+	List<RoomSaint> findAll();
+}
