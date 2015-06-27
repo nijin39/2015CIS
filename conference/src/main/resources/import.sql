e713ce7effdf1461258fc87c85223190ed8974e0
Kim Jong IL <nijin39@gmail.com>
2015-06-27 15:37:23 +0900

    초기 import DATA 수정

--- conference/src/main/resources/import.sql
+++ conference/src/main/resources/import.sql
@@ -1,9 +1,3 @@
-insert into groups(group_name, thread_pool_size) values ('GROUP1',5);
-insert into groups(group_name, thread_pool_size) values ('GROUP2',5);
-insert into groups(group_name, thread_pool_size) values ('GROUP3',5);
-insert into groups(group_name, thread_pool_size) values ('GROUP4',5);
-insert into groups(group_name, thread_pool_size) values ('GROUP5',5);
-
 insert into local(local_name, plocal_id, local_elder) values ('용산',0,'주평문');
 insert into local(local_name, plocal_id, local_elder) values ('용산',1,'김경태');
 insert into local(local_name, plocal_id, local_elder) values ('동작',1,'원종성');
